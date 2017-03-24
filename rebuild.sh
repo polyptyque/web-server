64452c8240acfed898886d34eae1b8725216056e
Arthur Violy <arthur@violy.net>
2017-03-24 23:24:03 +0100

    Rebuild script echo

--- rebuild.sh
+++ rebuild.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+echo "Pull the last commit from current branch..."
+
 git pull
+
+echo "Build the docker image, with latest modifictions..."
 
 docker build -t polyptyque:latest .
 
+echo "Stop and remove the container..."
+
 docker rm -f polyptyque.photo
 
+echo "Run the container..."
+
 docker run -d -e VIRTUAL_HOST=polyptyque.photo --name polyptyque.photo --restart=always polyptyque:latest
+
+echo "Done."
