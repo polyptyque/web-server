42f03f1b815d342589cd8d7570278bb82bccebc3
Arthur Violy <arthur@violy.net>
2017-06-15 00:29:14 +0200

    extend

--- index.js
+++ index.js
@@ -377,7 +377,7 @@ function ListAllShots(req,res,next){
     var connection = initializeConnection();
     connection.query("SELECT * FROM `shot` WHERE `enabled` = 1", function(err, results){
         if(err) return res.status(500).send("MySQLError:",err.toString());
-        res.render('list-all', _(config).extend({results:results, scripts:["list-all.js"], bodyClasses:['list-all']}))
+        res.render('list-all', _.extend({results:results, scripts:["list-all.js"], bodyClasses:['list-all']},config))
         connection.end();
     });
 }
@@ -392,7 +392,7 @@ function Demo(req, res, next) {
     if(A && B){
         imgBaseUrl = 'mixes/'+A+'-'+B+'-';
     }
-    res.render('demo', _(config).extend({imgBaseUrl:imgBaseUrl, scripts:["demo.js"], bodyClasses:['demo']}));
+    res.render('demo', _.extend({imgBaseUrl:imgBaseUrl, scripts:["demo.js"], bodyClasses:['demo']},config));
 }
 app.get('/demo', Demo);
 app.get('/demo-mix-:a-:b', Demo);
@@ -402,7 +402,7 @@ function Preview(req, res, next) {
     console.log('Demo. '+req.originalUrl);
     var imgBaseUrl = 'img/demo/',
         uid = req.params.uid;
-    res.render('preview', _(config).extend({uid:uid,scripts:["preview.js"], bodyClasses:['demo']}));
+    res.render('preview', _.extend({uid:uid,scripts:["preview.js"], bodyClasses:['demo']},config));
 }
 app.get('/preview', Preview);
 app.get('/preview-:uid', Preview);
@@ -412,7 +412,7 @@ app.get('/preview-:uid', Preview);
 function Home(req, res, next) {
     console.log('Home.');
     console.log(req.body);
-    res.render('home', _(config).extend({scripts:scripts, bodyClasses:['home']}));
+    res.render('home', _.extend({scripts:scripts, bodyClasses:['home']},config));
 }
 app.get('/', Home);
 app.post('/', Home);
@@ -424,7 +424,7 @@ app.use(express.static('public'));
 
 // 404
 app.use(function(req, res, next) {
-    res.status(404).render('not-found',_(config).extend({title:'404 non trouvé',bodyClasses:['404']}));
+    res.status(404).render('not-found',_.extend({title:'404 non trouvé',bodyClasses:['404']},config));
 });
 
 // Server
