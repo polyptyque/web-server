3e14a07d09b75a8cd1ace5e4a645e401c2e4ccc9
Arthur Violy <arthur@violy.net>
2018-01-17 01:17:15 +0100

    add video demo

--- rebuild.sh
+++ rebuild.sh
@@ -12,13 +12,13 @@ echo -e "$Green> Build the docker image, with latest modifictions... $Color_Off"
 
 docker build -t polyptyque:latest .
 
-echo -e "$Red> Stop and remove the container... $Color_Off"
+#echo -e "$Red> Stop and remove the container... $Color_Off"
 
-docker rm -f polyptyque.photo
+#docker rm -f polyptyque.photo
 
-echo -e "$Green> Run the container... $Color_Off"
+#echo -e "$Green> Run the container... $Color_Off"
 
-docker run -d --link gramme2-mysql:mysql -e VIRTUAL_HOST=polyptyque.photo --name polyptyque.photo --restart=always -v $(pwd)/uploads:/usr/src/app/uploads -v $(pwd)/mixes:/usr/src/app/mixes polyptyque:latest
+#docker run -d --link gramme2-mysql:mysql -e VIRTUAL_HOST=polyptyque.photo --name polyptyque.photo --restart=always -v $(pwd)/uploads:/usr/src/app/uploads -v $(pwd)/mixes:/usr/src/app/mixes polyptyque:latest
 # docker run --link gramme2-mysql:mysql -e VIRTUAL_HOST=polyptyque.photo --name polyptyque.photo --restart=always -v $(pwd)/uploads:/usr/src/app/uploads -v $(pwd)/mixes:/usr/src/app/mixes polyptyque:latest
 
-echo -e "$Green> Done.$Color_Off"
+#echo -e "$Green> Done.$Color_Off"
